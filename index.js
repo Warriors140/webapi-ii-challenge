f79d20b2f097ed979e199a0f370ef5adbcfa28dc
Randy Warrington <warrington.randy@outlook.com>
2019-08-15 21:02:25 -0400

    updated project

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const server = require('./server');
 
+const port = process.env.PORT || 5000;
 
-server.listen(5000, () => {
-    console.log('Server is running on port 5000...');
+
+server.listen(port, () => {
+    console.log(`Server is running on Port ${port}...`);
 });
